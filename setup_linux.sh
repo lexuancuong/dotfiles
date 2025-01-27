163b18f235ff518ef4e16a75f210543074d732c3
lexuancuong <cuong.le@parcelperform.com>
2025-01-27 11:42:18 +0700

    correct lazygit

--- setup_linux.sh
+++ setup_linux.sh
@@ -54,7 +54,10 @@ fi
 
 if [ "$(is_installed lazygit)" == "0" ]; then
   echo "Starting lazygit installation..."
-  apt install lazygit -y
+  LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+  curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+  tar xf lazygit.tar.gz lazygit
+  install lazygit -D -t /usr/local/bin/
   echo "✓ lazygit installed successfully"
 fi
 
