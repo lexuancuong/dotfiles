920e26b300c95f06086802ba7d879f0647623e26
lexuancuong <cuongle.software@gmail.com>
2025-10-11 19:04:30 +0700

    Add missing brew installation commands

--- packages/brew/install_brew.sh
+++ packages/brew/install_brew.sh
@@ -1,5 +1,8 @@
 if ! is_installed brew; then
   echo " Installing Homebrew..."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  echo >> /Users/lexuancuong/.zprofile
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/lexuancuong/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
 fi
 print_package_version brew
