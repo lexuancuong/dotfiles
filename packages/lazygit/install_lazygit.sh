1be0c25feeb3a429ee1e2634ea01e15aadfa4178
lexuancuong <cuongle.software@gmail.com>
2025-10-11 21:09:24 +0700

    Move lazygit to the config dir and export the path into the zshrc file

--- packages/lazygit/install_lazygit.sh
+++
@@ -1,7 +0,0 @@
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-print_package_version lazygit
-# Create LazyGit config directory
-CUSTOM_LG_FILE="$SCRIPT_DIR/config.yml"
-SYSTEM_LG_FILE="$(lazygit --print-config-dir)/config.yml"
-create_symlink "$CUSTOM_LG_FILE" "$SYSTEM_LG_FILE"
